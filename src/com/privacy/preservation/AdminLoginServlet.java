53cdab8bec27a7b09aac6bb292c4729029a396a9
gshivam63 <gshivam63@gmail.com>
2018-03-25 12:00:57 +0530

    Fourth commit (Home Page by Dhar)

--- src/com/privacy/preservation/AdminLoginServlet.java
+++ src/com/privacy/preservation/AdminLoginServlet.java
@@ -1,17 +1,43 @@
 package com.privacy.preservation;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
 @WebServlet(name = "AdminLoginServlet",urlPatterns = {"/adminloginservlet"})
 public class AdminLoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+        final String DB_URL="jdbc:mysql://localhost/admindetails";
+        //  Database credentials
+        final String USER = "root";
+        final String PASS = "complicated";
+        /* Set response content type */
+        response.setContentType("text/html");
+        try {
+            Class.forName(JDBC_DRIVER);
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT username,password from login_details";
+            ResultSet rs = stmt.executeQuery(sql);
+            // Extract data from result set
+            while(rs.next()){
+                System.out.println(rs.getString("username"));
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -1,16 +1,55 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: shivam
-  Date: 3/19/2018
-  Time: 4:33 PM
-  To change this template use File | Settings | File Templates.
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Anonymize your data</title>
+
+	<!-- Google Fonts -->
+	<!-- Custom Stylesheet -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+</head>
+
+<body>
+	<div class="container">
+		<div  class="login-box">
+			<div class="box-header" style="background-color: #665851">
+				<h2>Log In</h2>
+			</div>
+			<label for="username">Admin Name</label>
+			<br/>
+			<input type="text" id="username">
+			<br/>
+			<label for="password">Password</label>
+			<br/>
+			<input type="password" id="password">
+			<br/>
+			<button type="submit">Sign In</button>
+			<br/>
+		</div>
+	</div>
+</body>
+
+<script>
+	$(document).ready(function () {
+    	$("input:text:visible:first").focus();
+	});
+	$('#username').focus(function() {
+		$('label[for="username"]').addClass('selected');
+	});
+	$('#username').blur(function() {
+		$('label[for="username"]').removeClass('selected');
+	});
+	$('#password').focus(function() {
+		$('label[for="password"]').addClass('selected');
+	});
+	$('#password').blur(function() {
+		$('label[for="password"]').removeClass('selected');
+	});
+</script>
+
 </html>
